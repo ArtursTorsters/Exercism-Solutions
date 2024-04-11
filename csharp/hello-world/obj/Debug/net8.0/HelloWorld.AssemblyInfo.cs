c2c7299c88a99d5fe537b76c71f1f67c3ed63da1
Arturs Torsters <arrturs.torsters@gmail.com>
2024-04-11 09:55:12 +0300

    clock.ts

--- csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+447b4752f469ac5fd89741e435d795288f036049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46ecc5e7fdfe91b1b1145d9b15c18baa8bcb902")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
