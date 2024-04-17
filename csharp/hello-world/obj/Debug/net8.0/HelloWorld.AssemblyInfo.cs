f190d9e2ee029816a54ca25dc323dcf1dcff8e1b
Arturs Torsters <arrturs.torsters@gmail.com>
2024-04-17 15:09:41 +0300

    Collatz conjecture.

--- csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94627f2b8388319719fd5245357323219b9ab336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0707c9580f009fb5b4a53f6aa8d5054533ad9ad9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
