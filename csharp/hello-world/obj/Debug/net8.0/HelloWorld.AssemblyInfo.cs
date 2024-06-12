42578e18d63362bd8883ebab7c3d955ccc4d7a6c
Arturs Torsters <arrturs.torsters@gmail.com>
2024-06-12 15:01:19 +0300

    minesweeper

--- csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d2f129b8e31ad55a795a9b91361ea2d14395f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21fee2f15c402d3973a0b0d3f1e03a9b0808e6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
