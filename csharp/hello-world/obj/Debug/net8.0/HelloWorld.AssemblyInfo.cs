5f6ea944a76bbdb12ff3b9635edc702d31f0d9ff
Arturs Torsters <arrturs.torsters@gmail.com>
2024-06-17 15:48:58 +0300

    cipher

--- csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42578e18d63362bd8883ebab7c3d955ccc4d7a6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65125a3236b950d7c277173e5a8fe1d3b1a5906a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
