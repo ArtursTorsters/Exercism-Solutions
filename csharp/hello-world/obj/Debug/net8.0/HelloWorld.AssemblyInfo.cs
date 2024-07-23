fe2fee1d4edb243588ae632ec2ed944d6ce74942
Arturs Torsters <arrturs.torsters@gmail.com>
2024-07-23 16:00:39 +0300

    php

--- csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc100bdfdc3a2dac4c203fd384ff166d48fdcf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a291fb415261a11bb8180bf48aaa9b18a28c7a58")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
